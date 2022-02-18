07b74756b14c3a2e11d9cfab169d962f56cee369
Kritika Naagar <kritikanaagar15@gmail.com>
2022-02-18 22:38:53 +0530

    basic setup

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => { 
+    res.sendFile(process.cwd() + '/public/views/index.html')
+});
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => { 
+    console.log(`Listening on port ${port}`)
+})
